0c665a2245816a05afc1f1eeb1ff47ea059cad44
Vincent Sparkes <Vincent Sparkes>
2017-02-08 22:02:06 +0000

    Fixing some component styles, rudimentary search!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from "rxjs";
+import { NgModel } from "@angular/forms";
+import { Observable, Subject } from "rxjs";
 
 import { BookmarkStore, BookmarkTreeNode } from "./bookmarks/bookmark.store";
 import { MetadataProvider } from "./metadata.provider";
@@ -13,15 +14,35 @@ export class AppComponent implements OnInit {
 
   public bookmarks: Observable<BookmarkTreeNode[]>;
 
+  public searchTerm: Subject<string>;
+
+  public filteredBookmarks: Observable<BookmarkTreeNode[]>;
+
   constructor(
     private _bookmarkStore: BookmarkStore,
     private _metadataProvider: MetadataProvider
   ) {
-
+    
   }
 
   public ngOnInit(): void {
     this.bookmarks = this._bookmarkStore.bookmarks;
+    this.searchTerm = new Subject<string>();
+
+    this.filteredBookmarks = this.searchTerm
+      .startWith("")
+      .debounceTime(300)
+      .distinctUntilChanged()
+      .combineLatest(this.bookmarks)
+      .map(x => {
+        if(x[0] === "") {
+          return x[1];
+        }
+
+        return x[1].filter(y => y.title.toLowerCase().indexOf(x[0].toLowerCase()) > -1);
+      })
+      .publish()
+      .refCount();
   }
 
   public isImage(bookmark: BookmarkTreeNode): boolean {
@@ -34,7 +55,7 @@ export class AppComponent implements OnInit {
   public getMetadata(bookmark: BookmarkTreeNode): void{
     this._metadataProvider.getMetadata(bookmark.url)
       .subscribe(x => {
-        
+
       });
   }
 
